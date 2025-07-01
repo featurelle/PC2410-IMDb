4fa39cb98d1a0426f52c288a2a29065e3e9e8a3b
featurelle <s3m.channel@gmail.com>
2025-07-01 10:59:49 +0200

    removed redundant autofield (it's in settings), removed celery tasks

--- root/imdb/apps.py
+++ root/imdb/apps.py
@@ -2,9 +2,7 @@ from django.apps import AppConfig
 
 
 class ImdbConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'imdb'
 
     def ready(self):
         from . import signals
-        from . import tasks
